210c17e0431cff9fb1f80557f937b665e7cbe320
thaonee0 <phuongthaopham4814@gmail.com>
2024-01-16 12:33:07 +0700

    first commit

---
+++ src/services/userService.js
@@ -0,0 +1,12 @@
+import axios from '../axios';
+
+const handleLoginApi = (userEmail, userPassword) => {
+    return axios.post('/api/login', {email: userEmail, password: userPassword});
+}
+
+const getAllUsers = (inputId) => {
+    //template string
+    return axios.get(`/api/get-all-users?id=${inputId}`)
+}
+
+export { handleLoginApi, getAllUsers }
